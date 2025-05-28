9617b8510ec1909dd6bfcc259a951b54fb308b8a
Rishav <pinkyjaiswal7008@gmail.com>
2025-05-28 18:54:31 +0530

    server-search

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,12 +1,28 @@
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import { ChannelType } from "@prisma/client";
+import { ChannelType, MemberRole } from "@prisma/client";
 import { redirect } from "next/navigation";
 import { ServerHeader } from "./server-header";
+import { Hash, Mic, ShieldAlert, ShieldCheck, Video } from "lucide-react";
+import { ServerSearch } from "./server-search";
 
 interface ServerSidebar {
   serverId?: string;
 }
+
+const iconMap = {
+  [ChannelType.TEXT]: <Hash className="mr-2 h-4 w-4" />,
+  [ChannelType.AUDIO]: <Mic className="mr-2 h-4 w-4" />,
+  [ChannelType.VIDEO]: <Video className="mr-2 h-4 w-4" />,
+};
+
+const roleIconMap = {
+  [MemberRole.GUEST]: null,
+  [MemberRole.MODERATOR]: (
+    <ShieldCheck className="h-4 w-4 mr-2 text-indigo-500" />
+  ),
+  [MemberRole.ADMIN]: <ShieldAlert className="h-4 w-4 mr-2 text-rose-500" />,
+};
 
 export const ServerSidebar = async ({ serverId }: ServerSidebar) => {
   const profile = await currentProfile();
@@ -59,6 +75,48 @@ export const ServerSidebar = async ({ serverId }: ServerSidebar) => {
   return (
     <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5] ">
       <ServerHeader server={server} role={role} />
+      <div>
+        <ServerSearch
+          data={[
+            {
+              label: "Text Channels",
+              type: "channel",
+              data: textChannels?.map((channel) => ({
+                id: channel.id,
+                name: channel.name,
+                icon: iconMap[channel.type],
+              })),
+            },
+            {
+              label: "Audio Channels",
+              type: "channel",
+              data: audioChannels?.map((channel) => ({
+                id: channel.id,
+                name: channel.name,
+                icon: iconMap[channel.type],
+              })),
+            },
+            {
+              label: "Video Channels",
+              type: "channel",
+              data: videoChannels?.map((channel) => ({
+                id: channel.id,
+                name: channel.name,
+                icon: iconMap[channel.type],
+              })),
+            },
+            {
+              label: "Members",
+              type: "member",
+              data: members?.map((member) => ({
+                id: member.id,
+                name: member.profile.name,
+                icon: roleIconMap[member.role],
+              })),
+            },
+          ]}
+        />
+      </div>
     </div>
   );
 };
